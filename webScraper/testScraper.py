8470a4aeb8e8bb8c9ecfb4e3f7f7b69a44f8ecf6
Tom <1991tnguyen@gmail.com>
2022-08-11 12:28:25 +0800

    added type checking tests

--- webScraper/testScraper.py
+++ webScraper/testScraper.py
@@ -3,21 +3,30 @@ import unittest
 import pandas as pd
 
 class TestScraper(unittest.TestCase):
+    name_addr = dict()
     df = pd.DataFrame()
     headers = None
 
     DATA_FILE = "./test_files/Food Business Listing 2021.22 - CoA Summary.xls"
 
     def setUp(self):
-        self.df = scraper.get_business_name_add(self.DATA_FILE)
-        self.headers = scraper.read_file(self.DATA_FILE).keys()
+        self.name_addr = scraper.get_business_name_add(self.DATA_FILE)
+        self.df =scraper.read_file(self.DATA_FILE)
+        self.headers = self.df.keys()
+
+
+    def test_correct_type(self):
+        self.assertTrue(isinstance(self.name_addr, type(dict())), msg=f"Business name and address object is NOT a dictionary type")
+        self.assertTrue(isinstance(self.headers, pd.Index), msg=f"Headers object is NOT a Pandas Index type")
+        self.assertTrue(isinstance(self.df, pd.DataFrame), msg=f"df object is NOT a Pandas DataFrame type")
+
 
     def test_get_name_addr(self):
-        self.assertEqual(self.df["Armadale Primary School Canteen"], "1 Carradine Road BEDFORDALE WA 6112")
-        self.assertEqual(self.df["Snugglepot Child Care Centre"], "48 Poad Street SEVILLE GROVE WA 6112")
-        self.assertEqual(self.df["Ashlee's Family Daycare"], "")
-        self.assertEqual(self.df["S and C Fiolo"], "1311 Brookton Highway KARRAGULLEN WA 6111")
-        self.assertEqual(self.df["Zeez Beez"], "")
+        self.assertEqual(self.name_addr["Armadale Primary School Canteen"], "1 Carradine Road BEDFORDALE WA 6112")
+        self.assertEqual(self.name_addr["Snugglepot Child Care Centre"], "48 Poad Street SEVILLE GROVE WA 6112")
+        self.assertEqual(self.name_addr["Ashlee's Family Daycare"], "")
+        self.assertEqual(self.name_addr["S and C Fiolo"], "1311 Brookton Highway KARRAGULLEN WA 6111")
+        self.assertEqual(self.name_addr["Zeez Beez"], "")
 
 
     def test_headers(self):
