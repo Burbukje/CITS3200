97eee6d1f94270a6f863797c8dbbb1b85d98806c
rodneyCham <22209318@student.uwa.edu.au>
2022-08-17 11:21:21 +0800

    food business db insert statements

---
+++ insert statements.sql
@@ -0,0 +1,11 @@
+INSERT INTO Food_Business (local_government_area, red_cap_id, collection_year, business_name, original_lga_provided_address, new_street_number, new_street_name, new_street_type, new_suburb, new_postcode, contact_1, contact_2, email, website, menu_provided, children_menu_provided, mon_open, mon_close, mon_total_hrs, tue_open, tue_close, tue_total_hrs, wed_open, wed_close, wed_total_hrs, thu_open, thu_close, thu_total_hrs, fri_open, fri_close, fri_total_hrs, sat_open, sat_close, sat_total_hrs, sun_open, sun_close, sun_total_hrs, total_weekdays_hrs, total_weekend_hrs)
+ VALUES ('Plantagenet, Shire of
+', 15659, 2022, 'Gilbert Wines', 
+'30138 Albany Highway, Kendenup', 30138, 'Albany', 'Highway', 
+'Kendenup', 6263,  0898514028, 1800708110, 'gilberts@agn.net.au', 'https://www.gilbertwines.com.au/', 
+1, 0, 
+'10:00:00', '17:00:00', '7:00:00', '0:00:00', '0:00:00', '0:00:00', 
+'0:00:00', '0:00:00', '0:00:00', '10:00:00', '17:00:00', '7:00:00', 
+'10:00:00', '17:00:00', '7:00:00', '10:00:00', '17:00:00', '7:00:00', 
+'10:00:00', '17:00:00', '7:00:00', 
+'21:00:00', '14:00:00');
