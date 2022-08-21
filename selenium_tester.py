e7811068bd95bfb5712b87b8ac58e4477147d8e5
twiggly <110087537+twiggly@users.noreply.github.com>
2022-08-21 14:26:36 +0800

    Selenium Tester Version 2 (reliable)
    This code visits a a google Maps URL and returns whether the restaurant has a photo labelled "Menu" in the photos section.
    For this code to work, Firefox must remain open on screen, i.e., the browser cannot be minimized to taskbar.

--- selenium_tester.py
+++ selenium_tester.py
@@ -2,10 +2,14 @@
 # Demonstrates how selenium can be used to extract menu data from Google Maps
 # Requires geckodriver: https://github.com/mozilla/geckodriver/releases
 # Requires Firefox: https://www.mozilla.org/en-US/firefox/new/
+# Note: In order for this code to work, the Firefox tab must remain open, e.i., it cannot be minimized
+# If a site cannot be reached or the elements in a site cannot be accessed, the site will be treated as if its menu was not available
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 URLs = {
     "https://www.google.com/maps/place/Tasty+Momo+Restaurant+Nepalese+%26+Indian+Cuisine/@-31.9067571,115.8731062,15z/data=!4m5!3m4!1s0x2a32b075d9e1d36f:0x5179bbaeb7d8693c!8m2!3d-31.9067574!4d115.8818606",
@@ -13,29 +17,37 @@ URLs = {
     "https://www.google.com/maps/place/Curry+Thieves/@-31.89532,115.871346,18.26z/data=!4m5!3m4!1s0x0:0x2765e7d7f965fa3b!8m2!3d-31.8959657!4d115.8714174"
 }
 
-print("Note: I do not know why the URLs are checked in random order.")
-print("The code should print 2 Trues and 1 False.")
+# for some reason, the URLs are checked in random order
+print("The code should print 2 Trues and 1 False in any order")
 
 # geckodriver.exe can be in either (1) system PATH (2) same folder as this script (3) same folder as python installation
 driver = webdriver.Firefox()
 # if the line above does not work, you can manually define where to find geckodriver.exe. For example:
 # driver = webdriver.Firefox(executable_path=r'C:\Users\bisho\Documents\geckodriver.exe')
 
-driver.implicitly_wait(4) # wait max 4 seconds to find an element not immediately available
-
 for URL in URLs:
-    driver.get(URL)
+    try:
+        driver.get(URL)
+    except:
+        print("Could not access URL. Ensure that device has stable internet connection.")
+        print(URL)
+
     answer = False
 
     try:
-        elements = driver.find_elements(By.CLASS_NAME, 'fontTitleSmall.fontTitleMedium')
-        for element in elements:
-            if element.text == "Menu":
+        side_panel = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.CLASS_NAME, 'DUwDvf.fontHeadlineLarge')))
+        side_panel.click() # interact with side panel so that photo name elements load
+
+        photo_names = WebDriverWait(driver, 5).until(EC.visibility_of_all_elements_located((By.CLASS_NAME, 'fontTitleSmall.fontTitleMedium')))
+        
+        for photo_name in photo_names:
+            if photo_name.text == "Menu":
                 answer = True
 
-    except NoSuchElementException:
-        print("Error: Cannot access Google Map's photos")
+    except TimeoutException:
+        print("TimoutException: Elements never became visible while checking URL. Ensure that Firefox is not minimized.")
+        print(URL)
     
-    print(answer)
+    print(f"Menu available: {answer}")
     
 driver.quit()
