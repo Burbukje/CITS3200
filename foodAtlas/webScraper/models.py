850b55d27aee2df4cfc84d8f5614b55dc2a1c586
Tom <1991tnguyen@gmail.com>
2022-08-28 18:10:02 +0800

    added business models

--- foodAtlas/webScraper/models.py
+++ foodAtlas/webScraper/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+
+class Collection_Year(models.Model):
+    year = models.IntegerField(primary_key=True)
+
+    def __str__(self):
+        return str(self.year)
+
+
+class Local_Government(models.Model):
+    year = models.ForeignKey(Collection_Year, on_delete=models.CASCADE)
+    local_government_area = models.CharField(max_length=128, primary_key=True)
+
+    def __str__(self):
+        return self.local_government_area
+
+
+class Business(models.Model):
+    local_government_area = models.ForeignKey(Local_Government, on_delete=models.CASCADE)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    business_name = models.CharField(max_length=128)
+    notes = models.TextField(max_length=256)
+
+
+class Contact_Details(models.Model):
+    business_id = models.ForeignKey(Business, on_delete=models.CASCADE)
+    longitude = models.DecimalField(max_digits=18, decimal_places=15)
+    latitude = models.DecimalField(max_digits=18, decimal_places=15)
+    parcels_address = models.CharField(max_length=128)
+    formatted_address = models.CharField(max_length=128)
+    phone = models.CharField(max_length=15)
+    website = models.CharField(max_length=128)
+    menu = models.BooleanField(default=False)
+    opening_hours = models.JSONField()
