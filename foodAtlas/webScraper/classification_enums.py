dd9e1e8ae59670b111bfd0cf5deeeffdc89a0092
Tom <1991tnguyen@gmail.com>
2022-08-30 10:29:34 +0800

    added class enums

---
+++ foodAtlas/webScraper/classification_enums.py
@@ -0,0 +1,93 @@
+from socket import CAN_EFF_FLAG
+from types import GenericAlias
+from django.db import models
+
+class Classification_Appendix(models.TextChoices):
+
+    A = 'A', ("Food Retail")
+    B = 'B', ("Food Service")
+    C = 'C', ("Charitable Food Provisions")
+    D = 'D', ("Food Productiond and Preperation")
+    E = 'E', ("Institutional Food") 
+    F = 'F', ("Accomadation and Recreation")
+
+    @classmethod
+    def choices(cls):
+        return[(item.value, item.name) for item in cls]
+
+
+#---------------NOT USED-------------------------
+class Category_A(models.TextChoices):
+    
+    SUPERMARKET = 1.00, ("Supermarket/Grocery Store")
+    CONVENIENCE = 2.00, ("Convenience Store")
+    WORLDFOOD   = 3.00, ("World/Ethnic Foods")
+    HEALTH      = 4.00, ("Health Food Store")
+    BUCTHER     = 5.00, ("Butcher/Poultry Store")
+    FISHMONGER  = 6.00, ("Fishmonger")
+    BAKERY      = 7.00, ("Bakery")
+    GREENGROCER = 8.00, ("Fruit and Veg / Greengrocer")
+    DELI        = 9.00, ("Delicatessen")
+    PATISSERIE  = 10.00, ("Pastry/Patisserie Store")
+    CONFECTIONARY = 11.00, ("Confectionary/Chocolate/Ice-Cream")
+    OTHER        = 12.00, ("Not otherwise specified")
+    LIQUOR      = 13.00, ("Liquor/Bottle Shop")
+    GENERAL     = 14.00, ("General Retail")
+
+    @classmethod
+    def choices(cls):
+        return[(item.value, item.name) for item in cls]
+
+
+class Category_B(models.TextChoices):
+
+
+    @classmethod
+    def choices(cls):
+        return[(item.value, item.name) for item in cls]
+
+
+class Category_C(models.TextChoices):
+
+    
+    @classmethod
+    def choices(cls):
+        return[(item.value, item.name) for item in cls]
+
+
+class Category_D(models.TextChoices):
+
+    
+    @classmethod
+    def choices(cls):
+        return[(item.value, item.name) for item in cls]
+
+
+class Category_E(models.TextChoices):
+
+    
+    @classmethod
+    def choices(cls):
+        return[(item.value, item.name) for item in cls]
+
+
+class Category_F(models.TextChoices):
+
+    
+    @classmethod
+    def choices(cls):
+        return[(item.value, item.name) for item in cls]
+
+
+class Sub_Category_One(models.TextChoices):
+    SUPERMARKET = 0.01, ("Supermarket")
+    DISCOUNT = 0.02, ("Discount Grocery Store")
+    WHOLESALE = 0.03, ("Wholesale Grocery Store")
+
+
+    @classmethod
+    def choices(cls):
+        return[(item.value, item.name) for item in cls]
+
+
+#--------------------------------------------------------------
