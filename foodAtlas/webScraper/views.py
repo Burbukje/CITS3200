3ce28b2605c3b528627fb1a98320ba676135ddd0
Tom <1991tnguyen@gmail.com>
2022-09-05 11:40:45 +0800

    added views

--- foodAtlas/webScraper/views.py
+++ foodAtlas/webScraper/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect
 
 # Create your views here.
 
-def login(request):
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("uploader")
+
     return render(request, "login.html")
 
 
+def logout_view(request):
+    logout(request)
+    return redirect("index")
+
+
 def uploader(request):
-    return render(request, "uploader.html")
+    if not request.user.is_authenticated:
+        return redirect("login")
+    else:
+        return render(request, "uploader.html")
