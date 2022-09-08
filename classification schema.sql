1e669c87bca3f85207299bc3e8369d5a08cd21fc
rodneyCham <22209318@student.uwa.edu.au>
2022-09-08 14:57:41 +0800

    changes to primary and foreign keys

--- classification schema.sql
+++ classification schema.sql
@@ -1,30 +1,28 @@
-CREATE TABLE Classification
+CREATE TABLE classification
 (
   classification_code CHAR NOT NULL,
   classification_type VARCHAR NOT NULL,
   description VARCHAR NOT NULL,
-  internal_id INT NOT NULL,
-  PRIMARY KEY (classification_code),
-  FOREIGN KEY (internal_id) REFERENCES business(internal_id)
+  PRIMARY KEY (classification_code)
 );
 
-CREATE TABLE Category
+CREATE TABLE category
 (
   category_type VARCHAR NOT NULL,
   category_code VARCHAR NOT NULL,
   description VARCHAR NULL,
   classification_code CHAR NOT NULL,
   PRIMARY KEY (category_code),
-  FOREIGN KEY (classification_code) REFERENCES Classification(classification_code)
+  FOREIGN KEY (classification_code) REFERENCES classification(classification_code)
 );
 
-CREATE TABLE Sub-category
+CREATE TABLE sub_category
 (
-  sub-category_type VARCHAR NOT NULL,
-  sub-category_code VARCHAR NOT NULL,
+  sub_category_type VARCHAR NOT NULL,
+  sub_category_code VARCHAR NOT NULL,
   description VARCHAR NOT NULL,
   examples VARCHAR NOT NULL,
   category_code VARCHAR NOT NULL,
-  PRIMARY KEY (sub-category_code),
-  FOREIGN KEY (category_code) REFERENCES Category(category_code)
+  PRIMARY KEY (sub_category_code),
+  FOREIGN KEY (category_code) REFERENCES category(category_code)
 );
