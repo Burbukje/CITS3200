7252f465cf30c03e3ed853fbc983013bbd989bb6
Bea <burbuqe.shaqiri98@gmail.com>
2022-09-18 14:46:50 +0800

    removed restriction

--- foodAtlas/foodAtlas/settings.py
+++ foodAtlas/foodAtlas/settings.py
@@ -122,7 +122,6 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-ALLOWED_HOSTS = ['herokuappname.herokuapp.com']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
