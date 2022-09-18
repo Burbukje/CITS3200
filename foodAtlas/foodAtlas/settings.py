83cbda59164af5701f19125083726ce783a6c570
Bea <burbuqe.shaqiri98@gmail.com>
2022-09-18 13:24:03 +0800

    added host to settings

--- foodAtlas/foodAtlas/settings.py
+++ foodAtlas/foodAtlas/settings.py
@@ -122,6 +122,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
+ALLOWED_HOSTS = ['herokuappname.herokuapp.com']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
