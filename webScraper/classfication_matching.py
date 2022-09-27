c0fd17d04a4e595f2ac343821eed200d86f4b018
Tarnage <70882644+Tarnage@users.noreply.github.com>
2022-09-27 16:33:34 +0800

    testing classification

--- webScraper/classfication_matching.py
+++ webScraper/classfication_matching.py
@@ -1,14 +1,25 @@
 from webScraper.models import Collection_Year, Contact_Details, Business, Local_Government, Classification
+import pandas as pd
+GB_TYPES = "webScraper/static/assets/GOOGLE_API_BUSSINESS_TYPE_NAMES.xlsx"
 
 
-def  db_add_possible_classification(classification, headers: list,matched) -> None:
+
+def  db_add_possible_classification(classification, matched) -> None:
 
     classification_name = matched
     classification.possible_classifications = classification_name
     classification.save()
 
+
+def  db_add_possible_cats(classification, matched) -> None:
+
+    categories_name = matched
+    classification.possible_categories = categories_name
+    classification.save()
+
+
 def get_match_sheet():
-    match_sheet = pd.read_excel('GOOGLE API BUSSINESS TYPE NAMES.xlsx',header=0,sheet_name = 'Only_matched')
+    match_sheet = pd.read_excel(GB_TYPES,header=0,sheet_name = 'Only_matched')
     match_sheet['GOOGLE API ALL BUSINESS TYPE'] = match_sheet['GOOGLE API ALL BUSINESS TYPE'].str.strip()
     match_sheet['GOOGLE API ALL BUSINESS TYPE'] = match_sheet['GOOGLE API ALL BUSINESS TYPE'].str.lower()
 
@@ -32,7 +43,7 @@ def classification_dictionarys(match_sheet):
     return classification,Category,Category_code,Sub_category,Sub_category_code
 
 
-def class_matching(business_type_list,match_sheet) :
+def class_matching(business_type_list,match_sheet):
 
     classification,Category,Category_code,Sub_category,Sub_category_code = classification_dictionarys(match_sheet)
   
@@ -42,14 +53,21 @@ def class_matching(business_type_list,match_sheet) :
     Sub_category_l = []
     Sub_category_code_l = []
 
-    for k in business_type_list :
-        key = k.strip()
-        classification_l.append(classification.get(key,(0,'undefined')))
-        Category_l.append(Category.get(key.strip(),(0,'undefined')))
-        Category_code_l.append(Category_code.get(key.strip(),(0,'undefined')))
-        Sub_category_l.append(Sub_category.get(key.strip(),(0,'undefined')))
-        Sub_category_code_l.append(Sub_category_code.get(key.strip(),(0,'undefined'))
-) 
+    for business_obj in business_type_list:
+        
+        google_types = business_obj.google_business_types
+        classification_obj = Classification.objects.filter(business_id=business_obj)[0]
+
+        db_add_possible_classification(classification_obj, "THIS IS A TEST")
+
+        print(google_types)
+        break
+
+        # classification_l.append(classification.get(key,(0,'undefined')))
+        # Category_l.append(Category.get(key.strip(),(0,'undefined')))
+        # Category_code_l.append(Category_code.get(key.strip(),(0,'undefined')))
+        # Sub_category_l.append(Sub_category.get(key.strip(),(0,'undefined')))
+        # Sub_category_code_l.append(Sub_category_code.get(key.strip(),(0,'undefined'))) 
 
 #remove na
     classification_l =[x for x in classification_l if x == x]
@@ -78,7 +96,21 @@ def class_matching(business_type_list,match_sheet) :
      'category_one':     [Category_l[1], Category_code_l[1]],
      'sub_category_one': [Sub_category_l[1],Sub_category_code_l[1]]}
    
-
     return result
+
+
+def get_lga_business():
+    # year = int(year)
+    # lga = lga.upper()
+
+    year = 2022
+    lga = "ARMADALE, CITY OF"
+
+    year_obj = Collection_Year.objects.filter(year=year)[0]
+    lga_object = Local_Government.objects.filter(local_government_area=lga, year=year_obj)[0]
+    query_set = Business.objects.filter(local_government_area=lga_object)
+
+    class_matching(query_set, get_match_sheet())
+
 
 
