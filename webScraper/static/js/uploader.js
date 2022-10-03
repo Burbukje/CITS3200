ca2de9625f38840b20df43b7a88722881957ce72
Tarnage <70882644+Tarnage@users.noreply.github.com>
2022-10-03 15:33:30 +0800

    removed modal

--- webScraper/static/js/uploader.js
+++ webScraper/static/js/uploader.js
@@ -95,38 +95,38 @@ function uploadFile(file, i) {
 */
 
 // Get the modal
-var modal = document.getElementById("myModal");
+//var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("upload-btn");
+// var btn = document.getElementById("upload-btn");
 
 // Get the <span> (x) element that closes the modal
-var cross = document.getElementsByClassName("close")[0];
+// var cross = document.getElementsByClassName("close")[0];
 
 // Get the cancel button that closes the modal
-var cancelButton = document.getElementById("cancel-btn");
+// var cancelButton = document.getElementById("cancel-btn");
 
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
 
 // When the user clicks on <span> (x) close the modal
-cross.onclick = function() {
-  modal.style.display = "none";
-}
+// cross.onclick = function() {
+//   modal.style.display = "none";
+// }
 
 // When the user clicks the button, close the modal
-cancelButton.onclick = function() {
-  modal.style.display ="none";
-}
+// cancelButton.onclick = function() {
+//   modal.style.display ="none";
+// }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
 
 // Button to show and hide password
 function showHidePassword() {
