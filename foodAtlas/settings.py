69f6728454cecf2f01a043c351545b7f4af62b87
rodneyCham <22209318@student.uwa.edu.au>
2022-10-09 11:55:06 +0800

    changed settings again : _psycopg2

--- foodAtlas/settings.py
+++ foodAtlas/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'foodAtlas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': BASE_DIR / 'db.sqlite3', # needs to be changed when postgresql file implemented
         'OPTIONS': {
             'service': 'my_service',
