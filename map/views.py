21258e87005527ec65fe6a7618428128f77d0d2d
Bea <burbuqe.shaqiri98@gmail.com>
2022-10-12 11:13:22 +0800

    removed commented out code

--- map/views.py
+++ map/views.py
@@ -7,7 +7,6 @@ import geopandas as gpd
 from django.http import JsonResponse
 from webScraper.models import Local_Government
 from django.core import serializers
-import json
 
 
 # Create your views here.
@@ -15,13 +14,10 @@ import json
 def jsondata(request):
     data = Local_Government.objects.all()
     with open(r'map/geoJSON/LGAs_with_classified_data.geojson', 'w') as out:
-        mast_point = serializers.serialize('json', data)
+        mast_point = serializers.serialize('geojson', data)
         out.write(mast_point)
     context = {'object': out}
     return HttpResponse(context, request)
-    #for index in range(3, len(data)):
-    #   for key in data[index]:
-    #       print(data[index][key])
 
 def index(request):
     # Creating starting location and zoom of displayed map
@@ -65,47 +61,26 @@ def create_lga_map():
     return map1
 
 def create_detailed_lga_map():
-    filename = "map/geoJSON/LGAs_with_classified_data.json"
+    filename = "map/geoJSON/LGA_Boundaries_Metro_Area.geojson"
     file = open(filename)
+    classified_data = read_geojson()
     df = gpd.read_file(file)
+    for key, value in classified_data.items():
+        if classified_data[key] == 
     map2 = df.explore()
 
     folium.LayerControl().add_to(map2)
     return map2
 
-# loop through the lgas
-# get the corresponding businesses 
-# apply the classification algorithm to every business
-# lga-business-clasification
-#classification_matching file groups the businesses based on google_business_type in business_model
 def read_geojson():
-    classified_data = jsondata(HttpResponse)
-    geo_file = "map/geoJSON/LGAs_with_classified_data.json"
+    geo_file = "map/geoJSON/LGAs_with_classified_data.geojson"
     with open(geo_file, "r") as f:
         lga_dict = json.load(f)
-    # lga_dict = json.load(geo_file)
-
-    
-    #iterate through the list of dicts
-    #in every dict, the first 3 elements will indicate id, number and name of lga
     
         
-
     all_lga = {}
     for geocode in lga_dict["features"]:
-        #for ind in range(len(classified_data)):
-            # for key in classified_data[ind]:
-                #something like if key == lga_name????
-                #or if classified_data["local_government_area"] == lga_name["name"]
-                #maybe lga_name = geocode["name"]["key"]
-                #print(classified_data[ind][key])
-                ##############################
-                #loop through the dictionary, starting at element 2
-                # if its key matches lga_name["name"]
-                #store the rest of the elements of that dict in a datastructure and append that to all_lga
-                #if not, move to the next list item
-                # if ind["local_government_area"] == lga_name["name"]:
-        lga_name = geocode["properties"]["name"]
+        lga_name = geocode["properties"]["local_government_area"]
         all_lga[lga_name] = geocode
 
     return all_lga
