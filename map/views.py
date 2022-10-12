3ef64c6ce370fb4a457e6d747b84ef203e2042a3
Bea <burbuqe.shaqiri98@gmail.com>
2022-10-13 07:09:32 +0800

    append data from the model to existing

--- map/views.py
+++ map/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 import folium
 import json
 import geopandas as gpd
+import pandas as pd
 
 from django.http import JsonResponse
 from webScraper.models import Local_Government
@@ -61,34 +62,34 @@ def index(request):
 #     return map1
 
 def create_detailed_lga_map():
+    lgas_count = Local_Government.objects.count()
     filename = "map/geoJSON/LGA_Boundaries_Metro_Area.geojson"
     classified_data = read_classified_geojson()
-    all_lga={}
-    datafile = json.load(filename)
-    LGA_properties = datafile["features"]["properties"]
-    if LGA_properties["name"] == classified_data["local_government_area"]:
-        LGA_properties.append(classified_data)
-        converted_data = json.dumps(LGA_properties)
-        #df = gpd.read_file(converted_data, driver='GeoJSON')
-        df = gpd.DataFrame(converted_data)
-        # df = gpd.GeoDataFrame.from_features(converted_data)
-        map2 = df.explore()
+    json_data = json.load(the_final_file)
+
+    for index in range(0, lgas_count):
+        if classified_data[index]['local_government_area'] == json_data["features"][index]["properties"]["name"]:
+            json_data["features"][index]["properties"] = classified_data[index]
+            json_data["features"] = json_data["features"][index]
+            print(json_data["features"])
+                
+
+
+    df = pd.DataFrame.from_dict(json_data)
+
+    map2 = df.explore()
 
     folium.LayerControl().add_to(map2)
     return map2
 
 def read_classified_geojson():
+    lgas_count = Local_Government.objects.count()
     geo_file = "map/geoJSON/LGAs_with_classified_data.geojson"
-    # with open(geo_file, "r") as f:
-    #     lga_dict = json.load(f) 
-    classified_LGA = json.load(geo_file)
-    #classifications is a dict
-    classifications = classified_LGA["features"]["properties"]
-
-
-    # all_lga = {}
-    # for geocode in lga_dict["features"]:
-    #     lga_name = geocode["properties"]["local_government_area"]
-    #     all_lga[lga_name] = geocode
-#change this to a geojson file
-    return classifications
+    f = open(geo_file)
+    #this is a weird format
+    json_file = json.load(f)
+    all_lga = {}
+    for index in range(0, lgas_count):
+        classifications = json_file['features'][index]['properties']
+        all_lga[index] = classifications
+    return all_lga
