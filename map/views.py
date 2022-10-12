65069f1dd340cca4102b5061aed7cbf840974aa3
Bea <burbuqe.shaqiri98@gmail.com>
2022-10-12 20:47:17 +0800

    append data to geojson

--- map/views.py
+++ map/views.py
@@ -31,56 +31,64 @@ def index(request):
 
     return render(request, "index.html", context)
 
-def create_lga_map():
-    # Location of Perth
-    perth = [-31.95249936453132, 115.86121640254342]
+# def create_lga_map():
+#     # Location of Perth
+#     perth = [-31.95249936453132, 115.86121640254342]
 
-    # Styles for GeoJson Map
-    border_styles = {
-        'color': 'red',
-        'weight': 2,
-        'fillColor': 'blue',
-        'fillOpacity': 0.1
-    } 
+#     # Styles for GeoJson Map
+#     border_styles = {
+#         'color': 'red',
+#         'weight': 2,
+#         'fillColor': 'blue',
+#         'fillOpacity': 0.1
+#     } 
 
-    # Creating starting location and zoom of displayed map
-    map1 = folium.Map(location=perth, zoom_start=12)
-    geo_dict = read_geojson()
+#     # Creating starting location and zoom of displayed map
+#     map1 = folium.Map(location=perth, zoom_start=12)
+#     geo_dict = read_geojson()
 
-    # GeoJson data pack LGA of Perth
-    for shire in geo_dict:
-        # GeoJson data pack LGA of Perth
-        folium.GeoJson(geo_dict[shire], 
-                        name=shire,
-                        style_function=lambda x:border_styles).add_to(map1)
+#     # GeoJson data pack LGA of Perth
+#     for shire in geo_dict:
+#         # GeoJson data pack LGA of Perth
+#         folium.GeoJson(geo_dict[shire], 
+#                         name=shire,
+#                         style_function=lambda x:border_styles).add_to(map1)
         
 
-    # Adds layer control to Map
-    folium.LayerControl().add_to(map1)
+#     # Adds layer control to Map
+#     folium.LayerControl().add_to(map1)
 
-    return map1
+#     return map1
 
 def create_detailed_lga_map():
     filename = "map/geoJSON/LGA_Boundaries_Metro_Area.geojson"
-    file = open(filename)
-    classified_data = read_geojson()
-    df = gpd.read_file(file)
-    for key, value in classified_data.items():
-        if classified_data[key] == 
-    map2 = df.explore()
+    classified_data = read_classified_geojson()
+    all_lga={}
+    datafile = json.load(filename)
+    LGA_properties = datafile["features"]["properties"]
+    if LGA_properties["name"] == classified_data["local_government_area"]:
+        LGA_properties.append(classified_data)
+        converted_data = json.dumps(LGA_properties)
+        #df = gpd.read_file(converted_data, driver='GeoJSON')
+        df = gpd.DataFrame(converted_data)
+        # df = gpd.GeoDataFrame.from_features(converted_data)
+        map2 = df.explore()
 
     folium.LayerControl().add_to(map2)
     return map2
 
-def read_geojson():
+def read_classified_geojson():
     geo_file = "map/geoJSON/LGAs_with_classified_data.geojson"
-    with open(geo_file, "r") as f:
-        lga_dict = json.load(f)
-    
-        
-    all_lga = {}
-    for geocode in lga_dict["features"]:
-        lga_name = geocode["properties"]["local_government_area"]
-        all_lga[lga_name] = geocode
+    # with open(geo_file, "r") as f:
+    #     lga_dict = json.load(f) 
+    classified_LGA = json.load(geo_file)
+    #classifications is a dict
+    classifications = classified_LGA["features"]["properties"]
 
-    return all_lga
+
+    # all_lga = {}
+    # for geocode in lga_dict["features"]:
+    #     lga_name = geocode["properties"]["local_government_area"]
+    #     all_lga[lga_name] = geocode
+#change this to a geojson file
+    return classifications
