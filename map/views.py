9acad2c1b524259ae82dd386d1e62c17c0f29d0f
Bea <burbuqe.shaqiri98@gmail.com>
2022-10-15 16:05:05 +0800

    attempts to convert json to geojson

--- map/views.py
+++ map/views.py
@@ -8,6 +8,8 @@ import pandas as pd
 from django.http import JsonResponse
 from webScraper.models import Local_Government
 from django.core import serializers
+from geojson import FeatureCollection
+import geojson
 
 
 # Create your views here.
@@ -42,21 +44,30 @@ def create_detailed_lga_map():
     
 
     for ind in range(0, lgas_count):
-        #here is a key error
         for val in json_data['features']:
             if 'name' in val['properties'].keys():
                 lga_name = val['properties']['name']
             if classified_data[ind]['local_government_area'] == lga_name:
                 val['properties']= classified_data[ind]
 
+    with open("map/geoJSON/jsonfile.json", 'w') as json_file:
+        json_file.seek(0)
+        json.dump(json_data, json_file)
+
+    #add it to a feature collection
+    feature_collection = FeatureCollection(json_data)
+
                 
-    df = gpd.GeoDataFrame.from_features(json_data)
+    df = gpd.GeoDataFrame.from_features(feature_collection)
+
+    # gdf = gpd.GeoDataFrame(df).set_geometry(json_data)
     # df = df.to_crs(epsg=4326)
     map2 = df.explore()
-    #update the data to he original file
-    # with open(filename, 'w') as outfile:
-    #     outfile.seek(0)
-    #     json.dump(json_data, outfile)
+    #update the data to the original file
+    #the problem is that the json_data is a string not a file
+    #instead of creating that 
+    new_geojson = create_updated_geojson()
+    
 
     folium.LayerControl().add_to(map2)
     return map2
@@ -66,15 +77,39 @@ def read_classified_geojson():
     geo_file = "map/geoJSON/LGAs_with_classified_data.geojson"
 
     f = open(geo_file)
-    #this is a weird format
     json_file = json.load(f)
     all_lga = {}
     for index in range(0, lgas_count):
         classifications = json_file['features'][index]['properties']
-        all_lga[index] = classifications
+        if classifications != "0" or classifications != None:
+            all_lga[index] = classifications
     return all_lga
 
 
-def overwrite_geojson():
+def create_updated_geojson():
     """Update the geojson file everytime there is a change made to the database"""
-    print("blah")
+    input_file = json.load(open("map/geoJSON/jsonfile.json", "r", encoding="utf-8"))
+    #create the geojson format
+    # geojs={
+    #     "type": "FeatureCollection",
+    #     "features":[
+    #         {
+    #                 "type":"Feature",
+    #                 "geometry": {
+    #                 "type":"LineString",
+    #                 "coordinates":d["geojson"]["coordinates"],
+    #             },
+    #                 "properties":d,
+            
+    #             } for d in input_file
+    #         ]  
+    #     }
+    geojs = FeatureCollection(input_file)
+    output_file = open("map/geoJSON/updated_geojson.geojson", "w")
+    output_file.seek(0)
+    #add the json data
+    geojson.dump(geojs, output_file)
+
+
+
+        
