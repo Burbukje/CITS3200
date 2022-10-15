a3e129d3b0c650fd547922ed1b307a28eb365cca
Bea <burbuqe.shaqiri98@gmail.com>
2022-10-15 16:23:31 +0800

    applied the changes from the old layout

--- map/views.py
+++ map/views.py
@@ -5,7 +5,23 @@ import folium
 import json
 import geopandas as gpd
 
+
+from django.http import JsonResponse
+from webScraper.models import Local_Government
+from django.core import serializers
+from geojson import FeatureCollection
+import geojson
 # Create your views here.
+
+
+
+def jsondata(request):
+    data = Local_Government.objects.all()
+    with open('map/geoJSON/LGAs_with_classified_data.geojson', 'w') as out:
+        mast_point = serializers.serialize('geojson', data)
+        out.write(mast_point)
+    context = {'object': out}
+    return HttpResponse(context, request)
 
 def index(request):
 
@@ -53,24 +69,77 @@ def create_lga_map():
     return map1
 
 def create_detailed_lga_map():
+    lgas_count = Local_Government.objects.count()
     filename = "map/geoJSON/LGA_Boundaries_Metro_Area.geojson"
-    file = open(filename)
-    df = gpd.read_file(file)
+    classified_data = read_classified_geojson()
+    the_final_file = open(filename)
+    json_data = json.load(the_final_file)
+    
+
+    for ind in range(0, lgas_count):
+        for val in json_data['features']:
+            if 'name' in val['properties'].keys():
+                lga_name = val['properties']['name']
+            if classified_data[ind]['local_government_area'] == lga_name:
+                val['properties']= classified_data[ind]
+
+    with open("map/geoJSON/jsonfile.json", 'w') as json_file:
+        json_file.seek(0)
+        json.dump(json_data, json_file)
+
+    #add it to a feature collection
+    feature_collection = FeatureCollection(json_data)
+
+                
+    df = gpd.GeoDataFrame.from_features(feature_collection)
+
+    # gdf = gpd.GeoDataFrame(df).set_geometry(json_data)
+    # df = df.to_crs(epsg=4326)
     map2 = df.explore()
+    #update the data to the original file
+    #the problem is that the json_data is a string not a file
+    #instead of creating that 
+    new_geojson = create_updated_geojson()
+    
 
     folium.LayerControl().add_to(map2)
     return map2
 
-def read_geojson():
-    geo_file = "map/geoJSON/LGA_Boundaries_Metro_Area.geojson"
 
-    with open(geo_file, "r") as f:
-        lga_dict = json.load(f)
-        
+def read_classified_geojson():
+    lgas_count = Local_Government.objects.count()
+    geo_file = "map/geoJSON/LGAs_with_classified_data.geojson"
 
+    f = open(geo_file)
+    json_file = json.load(f)
     all_lga = {}
-    for geocode in lga_dict["features"]:
-        lga_name = geocode["properties"]["name"]
-        all_lga[lga_name] = geocode
-
+    for index in range(0, lgas_count):
+        classifications = json_file['features'][index]['properties']
+        if classifications != "0" or classifications != None:
+            all_lga[index] = classifications
     return all_lga
+
+
+def create_updated_geojson():
+    """Update the geojson file everytime there is a change made to the database"""
+    input_file = json.load(open("map/geoJSON/jsonfile.json", "r", encoding="utf-8"))
+    #create the geojson format
+    # geojs={
+    #     "type": "FeatureCollection",
+    #     "features":[
+    #         {
+    #                 "type":"Feature",
+    #                 "geometry": {
+    #                 "type":"LineString",
+    #                 "coordinates":d["geojson"]["coordinates"],
+    #             },
+    #                 "properties":d,
+            
+    #             } for d in input_file
+    #         ]  
+    #     }
+    geojs = FeatureCollection(input_file)
+    output_file = open("map/geoJSON/updated_geojson.geojson", "w")
+    output_file.seek(0)
+    #add the json data
+    geojson.dump(geojs, output_file)
